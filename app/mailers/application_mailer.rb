5a8f6106e9c04202b21adacee5a2860a80fde114
sweir27 <sweir27@gmail.com>
2017-06-12 13:13:03 -0400

    adds utm params to upload button

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,4 +7,12 @@ class ApplicationMailer < ActionMailer::Base
   def smtpapi(opts = {})
     headers['X-SMTPAPI'] = opts.to_json
   end
+
+  def utm_params(source:, campaign:)
+    {
+      utm_campaign: campaign,
+      utm_medium: 'email',
+      utm_source: source
+    }
+  end
 end
