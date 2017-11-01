fee73943a58e88a22944eee6bf1f1a7212526aa6
Sarah Weir <sweir27@gmail.com>
2017-11-01 15:22:07 -0400

    nullify primary_asset_id on destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171102162905) do
+=======
+ActiveRecord::Schema.define(version: 20171101190725) do
+>>>>>>> nullify primary_asset_id on destroy
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
