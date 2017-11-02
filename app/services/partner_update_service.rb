bcc56753d167147c86145b5b593a4d4073542f64
Sarah Weir <sweir27@gmail.com>
2017-11-02 15:27:08 -0400

    little refactors

--- app/services/partner_update_service.rb
+++ app/services/partner_update_service.rb
@@ -1,27 +1,21 @@
-require 'csv'
-
 module PartnerUpdateService
   class << self
     def update_partners_from_gravity
       Partner.all.each do |partner|
-        delay.update_partner_from_gravity(partner.id)
+        delay.update_partner!(partner.id)
       end
     end
 
-    def update_partner_from_gravity(partner_id)
-      partner = Partner.find(partner_id)
-      gravity_partner = Gravity.client.partner(id: partner.gravity_partner_id)._get
-      update_partner!(partner_id, gravity_partner.name)
-    end
-
-    def update_partner!(partner_id, new_partner_name)
-      partner = Partner.find(partner_id)
-      if partner.name == new_partner_name
-        Rails.logger.info "Skipping updating #{new_partner_name}: Name already up-to-date."
-      else
-        Rails.logger.info "Updating #{partner.name} to #{new_partner_name}."
-        partner.update_attributes!(name: new_partner_name)
+    def update_partner!(partner_id)
+      partner = Partner.find_by(id: partner_id)
+      if partner.blank?
+        Rails.logger.info "No partner found for #{partner_id}"
+        return
       end
+
+      gravity_partner = Gravity.client.partner(id: partner.gravity_partner_id)
+      new_partner_name = gravity_partner.name
+      partner.update_attributes!(name: new_partner_name) unless partner.name == new_partner_name
     end
   end
 end
