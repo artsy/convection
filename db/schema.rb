ae5d44ccb8cc09d29672cdddd7d2eec1be9f1e70
Sarah Weir <sweir27@gmail.com>
2017-11-08 12:58:08 -0500

    show status of submission

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171102162905) do
-=======
-ActiveRecord::Schema.define(version: 20171101190725) do
->>>>>>> nullify primary_asset_id on destroy
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
