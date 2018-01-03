5322833055df67838eb3a29d01094dcb2cf1e7f8
Sarah Weir <sweir27@gmail.com>
2018-01-03 14:18:03 -0500

    adds ability to create an offer from scratch

--- app/assets/javascripts/new_offer.js.coffee
+++ app/assets/javascripts/new_offer.js.coffee
@@ -1,26 +1,58 @@
 $ ->
 
   # Choose a partner to associate with an offer
-  formatPartner = (partner) ->
-    partner.name
+  if $('#new-offer-partner').length != 0
+    formatPartner = (partner) ->
+      partner.name
 
-  $('#new-offer-partner').select2(
-    ajax: {
-      url: '/admin/partners',
-      dataType: 'json',
-      data: (params) ->
-        term: params.term
-      processResults: (data, params) ->
-        results: data
-    },
-    allowClear: true,
-    placeholder: 'Select a partner',
-    minimumInputLength: 1,
-    width: 'element',
-    templateResult: formatPartner,
-    templateSelection: formatPartner
-  ).on('select2:select', (event) ->
-    $( '#partner_id' ).val( event.params.data.id )
-  ).on('select2:unselect', () ->
-    $( '#partner_id' ).val('')
-  ).val($('#partner_id').val()).trigger('change')
+    $('#new-offer-partner').select2(
+      ajax: {
+        url: '/admin/partners',
+        dataType: 'json',
+        data: (params) ->
+          term: params.term
+        processResults: (data, params) ->
+          results: data
+      },
+      allowClear: true,
+      placeholder: 'Select a partner',
+      minimumInputLength: 1,
+      width: 'element',
+      templateResult: formatPartner,
+      templateSelection: formatPartner
+    ).on('select2:select', (event) ->
+      $( '#partner_id' ).val( event.params.data.id )
+    ).on('select2:unselect', () ->
+      $( '#partner_id' ).val('')
+    ).val($('#partner_id').val()).trigger('change')
+
+  # Choose the submission to create an offer for
+  if $('#new-offer-submission').length != 0
+    formatSubmission = (submission) ->
+      if submission?.id != undefined && submission?.id?.length != 0
+        text = "##{submission.id} <b>#{submission.title}</b> (#{submission.state})"
+        submission = "<div class='new-offer-submission-thumbnail'><span><img src=#{submission.thumbnail}></img></span><span>#{text}</span></div>"
+        $(submission)
+      else
+        ''
+
+    $('#new-offer-submission').select2(
+      ajax: {
+        url: '/admin/submissions',
+        dataType: 'json',
+        data: (params) ->
+          term: params.term
+        processResults: (data, params) ->
+          results: data
+      },
+      allowClear: true,
+      placeholder: 'Select a submission',
+      minimumInputLength: 1,
+      width: 'element',
+      templateResult: formatSubmission,
+      templateSelection: formatSubmission
+    ).on('select2:select', (event) ->
+      $( '#submission_id' ).val( event.params.data.id )
+    ).on('select2:unselect', () ->
+      $( '#submission_id' ).val('')
+    ).val($('#submission_id').val()).trigger('change')
--- spec/controllers/admin/submissions_controller_spec.rb
+++ spec/controllers/admin/submissions_controller_spec.rb
@@ -22,22 +22,59 @@ describe Admin::SubmissionsController, type: :controller do
           }
         )
     end
-    context 'filtering the index view' do
+
+    context 'with many submissions' do
       before do
-        5.times { Fabricate(:submission, state: 'submitted') }
+        @submission1 = Fabricate(:submission, state: 'submitted', title: 'hi hi')
+        @submission2 = Fabricate(:submission, state: 'submitted', title: 'my artwork')
+        @submission3 = Fabricate(:submission, state: 'submitted', title: 'another artwork')
+        Fabricate(:submission, state: 'submitted', title: 'zzz')
+        Fabricate(:submission, state: 'submitted', title: 'aaa')
       end
-      it 'returns the first two submissions on the first page' do
-        get :index, params: { page: 1, size: 2 }
-        expect(assigns(:submissions).count).to eq 2
+
+      describe 'filtering the index view' do
+        it 'returns the first two submissions on the first page' do
+          get :index, params: { page: 1, size: 2 }
+          expect(assigns(:submissions).count).to eq 2
+        end
+        it 'paginates correctly' do
+          get :index, params: { page: 3, size: 2 }
+          expect(assigns(:submissions).count).to eq 1
+        end
+        it 'sets the artist details correctly' do
+          get :index
+          expect(assigns(:artist_details)).to eq('artist1' => 'Andy Warhol',
+                                                 'artist2' => 'Kara Walker')
+        end
       end
-      it 'paginates correctly' do
-        get :index, params: { page: 3, size: 2 }
-        expect(assigns(:submissions).count).to eq 1
-      end
-      it 'sets the artist details correctly' do
-        get :index
-        expect(assigns(:artist_details)).to eq('artist1' => 'Andy Warhol',
-                                               'artist2' => 'Kara Walker')
+
+      describe 'matching on the index' do
+        it 'returns the submissions that match as json' do
+          get :index, format: 'json', params: { term: 'hi' }
+          submissions = JSON.parse(response.body)
+          expect(submissions.length).to eq 1
+          expect(submissions.first['id']).to eq @submission1.id
+          expect(submissions.first['thumbnail']).to eq nil
+        end
+
+        it 'returns multiple submissions that match' do
+          get :index, format: 'json', params: { term: 'art' }
+          submissions = JSON.parse(response.body)
+          expect(submissions.length).to eq 2
+          expect(submissions.map { |sub| sub['id'] }).to eq [@submission2.id, @submission3.id]
+        end
+
+        it 'merges in the thumbnail url' do
+          Fabricate(:image,
+            submission: @submission1,
+            image_urls: { square: 'https://square.jpg', thumbnail: 'https://thumbnail-1.jpg' })
+
+          get :index, format: 'json', params: { term: 'hi' }
+          submissions = JSON.parse(response.body)
+          expect(submissions.length).to eq 1
+          expect(submissions.first['id']).to eq @submission1.id
+          expect(submissions.first['thumbnail']).to eq 'https://thumbnail-1.jpg'
+        end
       end
     end
   end
