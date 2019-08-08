169b97907691fde770cc3aa025b80af4b39a32f3
Will Doenlen <wdoenlen@gmail.com>
2019-08-08 10:56:21 -0400

    Use deleted_at datetime field instead of marked_as_deleted boolean

--- db/migrate/20190808142434_add_marked_as_deleted_to_submissions.rb
+++ db/migrate/20190808142434_add_marked_as_deleted_to_submissions.rb
@@ -1,5 +1,5 @@
 class AddMarkedAsDeletedToSubmissions < ActiveRecord::Migration[5.2]
   def change
-    add_column :submissions, :marked_as_deleted, :boolean
+    add_column :submissions, :deleted_at, :datetime
   end
 end
