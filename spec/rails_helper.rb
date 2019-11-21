87b02ab4212e6cc07a5aea79b0ae8499420a1249
Eve Essex <eve.essex@gmail.com>
2019-11-21 16:36:36 -0500

    fix chromedriver registration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'sidekiq/testing'
 require 'webmock/rspec'
+require 'rack_session_access/capybara'
+
+WebMock.disable_net_connect!(allow_localhost: true, allow: 'chromedriver.storage.googleapis.com')
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -32,7 +35,35 @@ RSpec.configure do |config|
 end
 
 Capybara.configure do |config|
-  config.javascript_driver = :selenium_chrome_headless
+  config.javascript_driver = :headless_chrome
   config.default_max_wait_time = 10
   config.ignore_hidden_elements = false
+end
+
+def prepare_chromedriver(selenium_driver_args)
+  if (driver_path = ENV['CHROMEDRIVER_PATH'])
+    service = Selenium::WebDriver::Service.new(path: driver_path, port: 9005)
+    selenium_driver_args[:service] = service
+  else
+    require 'webdrivers/chromedriver'
+  end
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
+  opts = Selenium::WebDriver::Chrome::Options.new(options: { 'w3c' => false })
+
+  opts.add_argument('--headless')
+  opts.add_argument('--no-sandbox')
+  opts.add_argument('--window-size=1440,900')
+
+  args = {
+    browser: :chrome,
+    options: opts,
+    desired_capabilities: caps
+  }
+
+  prepare_chromedriver(args)
+
+  Capybara::Selenium::Driver.new(app, args)
 end
