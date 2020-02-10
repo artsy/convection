41d8dac15afc24ec7f48106cb0a32c7094e1c4dc
Jon Allured <jon.allured@gmail.com>
2020-02-10 14:55:03 -0600

    Add nil check and spec for markdown support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,10 @@ module ApplicationHelper
     "#{currency} #{Money.new(price_cents, currency).format(no_cents: true)}"
   end
 
-  def markdown_formatted(note)
-    markdown = MarkdownParser.render(note)
+  def markdown_formatted(text)
+    return if text.blank?
+
+    markdown = MarkdownParser.render(text)
     markdown.html_safe # rubocop:disable Rails/OutputSafety
   end
 end
