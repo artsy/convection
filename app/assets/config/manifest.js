7cfa7ca952e5bba00d2797740a54a36850a2eef2
Christopher Pappas <damassi.pappas@gmail.com>
2020-02-27 15:12:45 -0800

    Add graphql assets to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,9 @@
 // Created this file based on these docs:
 // https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
-//
+
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link graphiql/rails/application.js
+//= link graphiql/rails/application.css
