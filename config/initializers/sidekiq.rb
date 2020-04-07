8fd40f91370208e2750323909995ec044b1f4f67
Jon Allured <jon.allured@gmail.com>
2020-04-07 11:24:38 -0500

    Improve local redis DX

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq::Extensions.enable_delay!
+
+if Rails.env.development?
+  redis_config = { url: "redis://localhost:6379/#{ENV.fetch('REDIS_DB', 0)}" }
+
+  Sidekiq.configure_server { |config| config.redis = redis_config }
+
+  Sidekiq.configure_client { |config| config.redis = redis_config }
+end
