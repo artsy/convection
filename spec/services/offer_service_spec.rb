d2b78316012d958737ba515cff086b545b5f4352
Jon Allured <jon.allured@gmail.com>
2020-05-13 15:39:00 -0500

    Do some reorganization of spec file

--- spec/services/offer_service_spec.rb
+++ spec/services/offer_service_spec.rb
@@ -8,8 +8,8 @@ describe OfferService do
   let(:partner) { Fabricate(:partner, name: 'Gagosian Gallery') }
   let(:submission) { Fabricate(:submission, state: submission_state) }
 
-  context 'create_offer' do
-    describe 'with a submission in submitted state' do
+  describe 'create_offer' do
+    context 'with a submission in submitted state' do
       let(:submission_state) { Submission::SUBMITTED }
 
       it 'updates the submission state to approved' do
@@ -23,7 +23,7 @@ describe OfferService do
       end
     end
 
-    describe 'with a submission in a draft state' do
+    context 'with a submission in a draft state' do
       let(:submission_state) { Submission::DRAFT }
 
       it 'raises an error' do
@@ -36,7 +36,7 @@ describe OfferService do
       end
     end
 
-    describe 'with a submission in a rejected state' do
+    context 'with a submission in a rejected state' do
       let(:submission_state) { Submission::REJECTED }
 
       it 'raises an error' do
@@ -49,7 +49,7 @@ describe OfferService do
       end
     end
 
-    describe 'with no initial partner submission' do
+    context 'with no initial partner submission' do
       let(:submission_state) { Submission::APPROVED }
 
       it 'creates a draft offer and a partner submission' do
@@ -69,7 +69,7 @@ describe OfferService do
       end
     end
 
-    describe 'with an initial partner submission' do
+    context 'with an initial partner submission' do
       let!(:partner_submission) do
         Fabricate(:partner_submission, partner: partner, submission: submission)
       end
@@ -111,7 +111,7 @@ describe OfferService do
     end
   end
 
-  context 'update_offer' do
+  describe 'update_offer' do
     it 'updates the offer with the new params' do
       offer =
         Fabricate(
@@ -153,186 +153,186 @@ describe OfferService do
       emails = ActionMailer::Base.deliveries
       expect(emails.length).to eq 0
     end
-  end
 
-  context 'with an offer' do
-    let(:partner) { Fabricate(:partner, name: 'Happy Gallery') }
-    let(:submission_state) { Submission::DRAFT }
-    let(:partner_submission) do
-      Fabricate(:partner_submission, partner: partner, submission: submission)
-    end
-    let(:offer) do
-      Fabricate(
-        :offer,
-        offer_type: 'purchase',
-        price_cents: 10_000,
-        state: Offer::DRAFT,
-        partner_submission: partner_submission
-      )
-    end
-
-    before do
-      stub_gravity_root
-      stub_gravity_user(id: offer.submission.user.gravity_user_id)
-      stub_gravity_user_detail(
-        email: 'michael@bluth.com', id: offer.submission.user.gravity_user_id
-      )
-      stub_gravity_artist(id: submission.artist_id)
-      stub_gravity_partner(id: partner.gravity_partner_id)
-      stub_gravity_partner_communications(
-        partner_id: partner.gravity_partner_id
-      )
-      stub_gravity_partner_contacts(
-        partner_id: partner.gravity_partner_id,
-        override_body: [
-          { email: 'contact1@partner.com' },
-          { email: 'contact2@partner.com' }
-        ]
-      )
-      allow(Convection.config).to receive(:offer_response_form_url).and_return(
-        'https://google.com/response_form?entry.1=SUBMISSION_NUMBER&entry.2=PARTNER_NAME'
-      )
-      allow(Convection.config).to receive(:auction_offer_form_url).and_return(
-        'https://google.com/offer_form?entry.1=SUBMISSION_NUMBER'
-      )
-    end
-
-    describe 'sending an offer' do
-      it 'sends an email to a user with offer information' do
-        OfferService.update_offer(offer, 'userid', state: Offer::SENT)
-        emails = ActionMailer::Base.deliveries
-        expect(emails.length).to eq 1
-        expect(emails.first.bcc).to eq(%w[consignments-archive@artsymail.com])
-        expect(emails.first.to).to eq(%w[michael@bluth.com])
-        expect(emails.first.from).to eq(%w[consign@artsy.net])
-        expect(emails.first.subject).to eq('Great news! You have a new offer.')
-
-        email_body = emails.first.html_part.body
-        expect(email_body).to include(
-          'Great news! A new offer has been made on your consignment.'
+    context 'with an offer' do
+      let(:partner) { Fabricate(:partner, name: 'Happy Gallery') }
+      let(:submission_state) { Submission::DRAFT }
+      let(:partner_submission) do
+        Fabricate(:partner_submission, partner: partner, submission: submission)
+      end
+      let(:offer) do
+        Fabricate(
+          :offer,
+          offer_type: 'purchase',
+          price_cents: 10_000,
+          state: Offer::DRAFT,
+          partner_submission: partner_submission
         )
-        expect(email_body).to include(
-          'The work will be purchased directly from you by the partner'
-        )
-        expect(email_body).to include('Happy Gallery')
-        expect(email_body).to include(
-          "https://google.com/response_form?entry.1=#{
-            submission.id
-          }&amp;entry.2=Happy%20Gallery"
-        )
-
-        offer.reload
-
-        expect(offer.state).to eq Offer::SENT
-        expect(offer.sent_by).to eq 'userid'
-        expect(offer.sent_at).to_not be_nil
       end
 
-      it 'does not send an email if the email has already been sent' do
-        offer.update!(sent_at: Time.now.utc)
-        OfferService.update_offer(offer, 'userid', state: Offer::SENT)
-        emails = ActionMailer::Base.deliveries
-        expect(emails.length).to eq 0
+      before do
+        stub_gravity_root
+        stub_gravity_user(id: offer.submission.user.gravity_user_id)
+        stub_gravity_user_detail(
+          email: 'michael@bluth.com', id: offer.submission.user.gravity_user_id
+        )
+        stub_gravity_artist(id: submission.artist_id)
+        stub_gravity_partner(id: partner.gravity_partner_id)
+        stub_gravity_partner_communications(
+          partner_id: partner.gravity_partner_id
+        )
+        stub_gravity_partner_contacts(
+          partner_id: partner.gravity_partner_id,
+          override_body: [
+            { email: 'contact1@partner.com' },
+            { email: 'contact2@partner.com' }
+          ]
+        )
+        allow(Convection.config).to receive(:offer_response_form_url).and_return(
+          'https://google.com/response_form?entry.1=SUBMISSION_NUMBER&entry.2=PARTNER_NAME'
+        )
+        allow(Convection.config).to receive(:auction_offer_form_url).and_return(
+          'https://google.com/offer_form?entry.1=SUBMISSION_NUMBER'
+        )
       end
-    end
 
-    describe 'introducing an offer' do
-      it 'sends an email saying the user is interested in the offer' do
-        OfferService.update_offer(offer, 'userid', state: Offer::REVIEW)
-        emails = ActionMailer::Base.deliveries
-        expect(emails.length).to eq 2
-        expect(emails.first.bcc).to eq(%w[consignments-archive@artsymail.com])
-        expect(emails.map(&:to).flatten).to eq(
-          %w[contact1@partner.com contact2@partner.com]
-        )
-        expect(emails.first.from).to eq(%w[consign@artsy.net])
-        expect(emails.first.subject).to eq(
-          'The consignor has expressed interest in your offer'
-        )
-        expect(emails.first.html_part.body).to include(
-          'Your offer has been reviewed, and the consignor has expressed interest your offer'
-        )
-        expect(emails.first.html_part.body).to include('Happy Gallery')
-        expect(emails.first.html_part.body).to_not include(
-                                                     'The work will be purchased directly from you by the partner'
-                                                   )
-        expect(offer.state).to eq Offer::REVIEW
-        expect(offer.review_started_at).to_not be_nil
-      end
-    end
+      context 'sending an offer' do
+        it 'sends an email to a user with offer information' do
+          OfferService.update_offer(offer, 'userid', state: Offer::SENT)
+          emails = ActionMailer::Base.deliveries
+          expect(emails.length).to eq 1
+          expect(emails.first.bcc).to eq(%w[consignments-archive@artsymail.com])
+          expect(emails.first.to).to eq(%w[michael@bluth.com])
+          expect(emails.first.from).to eq(%w[consign@artsy.net])
+          expect(emails.first.subject).to eq('Great news! You have a new offer.')
 
-    describe 'consigning an offer' do
-      context 'with an offer on a non-approved submission' do
-        it 'raises an error' do
-          expect {
-            OfferService.update_offer(offer, 'userid', state: Offer::CONSIGNED)
-          }.to raise_error(
-            OfferService::OfferError,
-            'Cannot complete consignment on non-approved submission'
+          email_body = emails.first.html_part.body
+          expect(email_body).to include(
+            'Great news! A new offer has been made on your consignment.'
           )
+          expect(email_body).to include(
+            'The work will be purchased directly from you by the partner'
+          )
+          expect(email_body).to include('Happy Gallery')
+          expect(email_body).to include(
+            "https://google.com/response_form?entry.1=#{
+              submission.id
+            }&amp;entry.2=Happy%20Gallery"
+          )
+
+          offer.reload
+
+          expect(offer.state).to eq Offer::SENT
+          expect(offer.sent_by).to eq 'userid'
+          expect(offer.sent_at).to_not be_nil
+        end
+
+        it 'does not send an email if the email has already been sent' do
+          offer.update!(sent_at: Time.now.utc)
+          OfferService.update_offer(offer, 'userid', state: Offer::SENT)
+          emails = ActionMailer::Base.deliveries
+          expect(emails.length).to eq 0
         end
       end
-      context 'with an offer on an approved submission' do
-        let(:submission_state) { Submission::APPROVED }
 
-        let(:ps) { Fabricate(:partner_submission, submission: submission) }
-        let(:consignable_offer) { Fabricate(:offer, partner_submission: ps) }
-        it 'sets fields on submission and partner submission' do
-          OfferService.update_offer(
-            consignable_offer,
-            'userid',
-            state: Offer::CONSIGNED
+      context 'introducing an offer' do
+        it 'sends an email saying the user is interested in the offer' do
+          OfferService.update_offer(offer, 'userid', state: Offer::REVIEW)
+          emails = ActionMailer::Base.deliveries
+          expect(emails.length).to eq 2
+          expect(emails.first.bcc).to eq(%w[consignments-archive@artsymail.com])
+          expect(emails.map(&:to).flatten).to eq(
+            %w[contact1@partner.com contact2@partner.com]
           )
-          expect(ActionMailer::Base.deliveries.count).to eq 0
+          expect(emails.first.from).to eq(%w[consign@artsy.net])
+          expect(emails.first.subject).to eq(
+            'The consignor has expressed interest in your offer'
+          )
+          expect(emails.first.html_part.body).to include(
+            'Your offer has been reviewed, and the consignor has expressed interest your offer'
+          )
+          expect(emails.first.html_part.body).to include('Happy Gallery')
+          expect(emails.first.html_part.body).to_not include(
+                                                       'The work will be purchased directly from you by the partner'
+                                                     )
+          expect(offer.state).to eq Offer::REVIEW
+          expect(offer.review_started_at).to_not be_nil
+        end
+      end
+
+      context 'consigning an offer' do
+        context 'with an offer on a non-approved submission' do
+          it 'raises an error' do
+            expect {
+              OfferService.update_offer(offer, 'userid', state: Offer::CONSIGNED)
+            }.to raise_error(
+              OfferService::OfferError,
+              'Cannot complete consignment on non-approved submission'
+            )
+          end
+        end
+        context 'with an offer on an approved submission' do
+          let(:submission_state) { Submission::APPROVED }
+
+          let(:ps) { Fabricate(:partner_submission, submission: submission) }
+          let(:consignable_offer) { Fabricate(:offer, partner_submission: ps) }
+          it 'sets fields on submission and partner submission' do
+            OfferService.update_offer(
+              consignable_offer,
+              'userid',
+              state: Offer::CONSIGNED
+            )
+            expect(ActionMailer::Base.deliveries.count).to eq 0
+            expect(ps.state).to eq 'open'
+            expect(ps.accepted_offer).to eq consignable_offer
+            expect(ps.partner_commission_percent).to eq consignable_offer
+                 .commission_percent
+            expect(
+              ps.submission.consigned_partner_submission
+            ).to eq consignable_offer.partner_submission
+            expect(consignable_offer.consigned_at).to_not be_nil
+          end
+        end
+      end
+
+      context 'rejecting an offer' do
+        it 'sends an email saying the offer has been rejected' do
+          OfferService.update_offer(offer, 'userid', state: Offer::REJECTED)
+          emails = ActionMailer::Base.deliveries
+          expect(emails.length).to eq 2
+          expect(emails.first.bcc).to eq(%w[consignments-archive@artsymail.com])
+          expect(emails.map(&:to).flatten).to eq(
+            %w[contact1@partner.com contact2@partner.com]
+          )
+          expect(emails.first.from).to eq(%w[consign@artsy.net])
+          expect(emails.first.subject).to eq(
+            'A response to your consignment offer'
+          )
+
+          email_body = emails.first.html_part.body
+          expect(email_body).to include(
+            'Your offer has been reviewed, and the consignor has rejected your offer.'
+          )
+          expect(email_body).to include('Happy Gallery')
+          expect(email_body).to_not include(
+                                      'The work will be purchased directly from you by the partner'
+                                    )
+          expect(email_body).to include(
+            "https://google.com/offer_form?entry.1=#{submission.id}"
+          )
+          expect(offer.state).to eq Offer::REJECTED
+          expect(offer.rejected_by).to eq 'userid'
+          expect(offer.rejected_at).to_not be_nil
+        end
+
+        it 'does not set consignment-related fields on an offer rejecton' do
+          OfferService.update_offer(offer, 'userid', state: Offer::REJECTED)
+          ps = offer.partner_submission
           expect(ps.state).to eq 'open'
-          expect(ps.accepted_offer).to eq consignable_offer
-          expect(ps.partner_commission_percent).to eq consignable_offer
-               .commission_percent
-          expect(
-            ps.submission.consigned_partner_submission
-          ).to eq consignable_offer.partner_submission
-          expect(consignable_offer.consigned_at).to_not be_nil
+          expect(ps.accepted_offer_id).to be_nil
+          expect(ps.partner_commission_percent).to be_nil
+          expect(ps.submission.consigned_partner_submission).to be_nil
         end
-      end
-    end
-
-    describe 'rejecting an offer' do
-      it 'sends an email saying the offer has been rejected' do
-        OfferService.update_offer(offer, 'userid', state: Offer::REJECTED)
-        emails = ActionMailer::Base.deliveries
-        expect(emails.length).to eq 2
-        expect(emails.first.bcc).to eq(%w[consignments-archive@artsymail.com])
-        expect(emails.map(&:to).flatten).to eq(
-          %w[contact1@partner.com contact2@partner.com]
-        )
-        expect(emails.first.from).to eq(%w[consign@artsy.net])
-        expect(emails.first.subject).to eq(
-          'A response to your consignment offer'
-        )
-
-        email_body = emails.first.html_part.body
-        expect(email_body).to include(
-          'Your offer has been reviewed, and the consignor has rejected your offer.'
-        )
-        expect(email_body).to include('Happy Gallery')
-        expect(email_body).to_not include(
-                                    'The work will be purchased directly from you by the partner'
-                                  )
-        expect(email_body).to include(
-          "https://google.com/offer_form?entry.1=#{submission.id}"
-        )
-        expect(offer.state).to eq Offer::REJECTED
-        expect(offer.rejected_by).to eq 'userid'
-        expect(offer.rejected_at).to_not be_nil
-      end
-
-      it 'does not set consignment-related fields on an offer rejecton' do
-        OfferService.update_offer(offer, 'userid', state: Offer::REJECTED)
-        ps = offer.partner_submission
-        expect(ps.state).to eq 'open'
-        expect(ps.accepted_offer_id).to be_nil
-        expect(ps.partner_commission_percent).to be_nil
-        expect(ps.submission.consigned_partner_submission).to be_nil
       end
     end
   end
