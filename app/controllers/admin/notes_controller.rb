efc8cc9d929858ce4493cb5ad76e5782978ec75d
Erik Stockmeier <erik.d.stock@gmail.com>
2020-06-30 14:48:48 -0400

    respond to feedback
    - better use of flash :alert and :notice, remove unused notes#new
    action, make body field required

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -4,13 +4,18 @@ module Admin
   class NotesController < ApplicationController
     def create
       @submission = Submission.find(note_params[:submission_id])
-      note = Note.create(gravity_user_id: @current_user, **note_params)
-      unless note.persisted?
-        flash[:notice] =
-          "Could not create note: #{note.errors.full_messages.join(', ')}"
-      end
+      note = Note.new(gravity_user_id: @current_user, **note_params)
 
-      redirect_to admin_submission_path(@submission)
+      if note.save
+        redirect_to admin_submission_path(@submission),
+                    notice: 'Note has successfully been created.'
+      else
+        redirect_to admin_submission_path(@submission),
+                    alert:
+                      "Could not create note: #{
+                        note.errors.full_messages.join(', ')
+                      }"
+      end
     end
 
     private
