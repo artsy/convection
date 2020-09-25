696fc10339e038f74e41652630e8d149477a75a8
Jian <jian-feng@artsymail.com>
2020-09-25 17:40:33 -0400

    Configure Event Service to take TLS flag from ENV.

--- config/initializers/artsy_eventservice.rb
+++ config/initializers/artsy_eventservice.rb
@@ -4,7 +4,7 @@ Artsy::EventService.configure do |config|
   config.app_name = 'artsy-convection'
   config.event_stream_enabled = true
   config.rabbitmq_url = ENV['RABBITMQ_URL']
-  config.tls = true
+  config.tls = ENV['RABBITMQ_TLS']
   config.tls_ca_certificate = Base64.decode64(ENV['RABBITMQ_CA_CERT'] || '')
   config.tls_cert = Base64.decode64(ENV['RABBITMQ_CLIENT_CERT'] || '')
   config.tls_key = Base64.decode64(ENV['RABBITMQ_CLIENT_KEY'] || '')
