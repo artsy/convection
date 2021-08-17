13ff6e2faed287091fd69c4bd8154c28f87c35b9
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-08-17 14:59:58 +0300

    Change the updater logic

--- app/controllers/api/consignments_controller.rb
+++ app/controllers/api/consignments_controller.rb
@@ -3,15 +3,29 @@
 module Api
   class ConsignmentsController < RestController
     before_action :require_authentication
-    before_action :set_submission, only: %i[update]
 
     def update
-      consignment = @submission.consigned_partner_submission
-      return unless consignment
+      # id in request == sale_id
+      sale_artworks = Gravity.client.sales(id: params(id)).sale_artworks
+      artworks_price = sale_artworks.map do |sale_artwork|
+        artwork_id = sale_artwork.artwork.id
+        price = sale_artwork.highest_bid.try(:amount_cents)
+        [artwork_id: artwork_id, price: price]
+      end
 
-      consignment.assign_attributes(consignment_params)
-      consignment.save!
-      render json: consignment.to_json, status: :created
+      consignments = update_price(artworks_price)
+      render json: { result: 'ok' }, status: :created
+    end
+
+    def update_price(artworks_price)
+      artworks_price.each do |artwork_price|
+        submission = Submission.find_by(source_atrwork_id: artwork_price[:artwork_id])
+        return unless submission
+
+        consignment = submission.consigned_partner_submission
+        consignment.assign_attributes(sale_price_cents: artwork_price[:price])
+        consignment.save!
+      end
     end
 
     def consignment_params
