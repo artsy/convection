9438f84624c7dbb5bfeb7b8cdb2004d24e024a71
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-08-20 16:38:30 +0300

    small fix, add specs

--- app/controllers/api/consignments_controller.rb
+++ app/controllers/api/consignments_controller.rb
@@ -6,12 +6,7 @@ module Api
 
     def update
       # id in request == sale_id
-      sale_artworks = Gravity.client.sales(id: params(id)).sale_artworks
-      artworks_price = sale_artworks.map do |sale_artwork|
-        artwork_id = sale_artwork.artwork.id
-        price = sale_artwork.highest_bid.try(:amount_cents)
-        [artwork_id: artwork_id, price: price]
-      end
+      artworks_price = fetch_artworks_price(params[:id])
 
       update_price(artworks_price)
       render json: { result: 'ok' }, status: :created
@@ -19,12 +14,22 @@ module Api
 
     def update_price(artworks_price)
       artworks_price.each do |artwork_price|
-        submission = Submission.find_by(source_atrwork_id: artwork_price[:artwork_id])
+        submission = Submission.find_by(source_artwork_id: artwork_price[:artwork_id])
         next unless submission
 
         consignment = submission.consigned_partner_submission
         consignment.assign_attributes(sale_price_cents: artwork_price[:price])
         consignment.save!
+      end
+    end
+
+    def fetch_artworks_price(id)
+      sale_artworks = Gravity.client.sales(id: id).sale_artworks
+
+      sale_artworks.map do |sale_artwork|
+        artwork_id = sale_artwork.artwork.id
+        price = sale_artwork.highest_bid.try(:amount_cents)
+        [artwork_id: artwork_id, price: price]
       end
     end
 
---
+++ spec/controllers/api/consignment_contoller_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Api::ConsignmentsController, type: :controller do
+  before do
+    allow_any_instance_of(Api::ConsignmentsController).to receive(
+      :require_authentication
+    )
+    allow_any_instance_of(Api::ConsignmentsController).to receive(:fetch_artworks_price).and_return(
+      [artwork_id: 1, price: 2]
+    )
+  end
+
+  describe '#update' do
+    let(:partner) { Fabricate(:partner, name: 'Gagosian Gallery') }
+    let(:submission) do 
+      Fabricate(
+        :submission,
+        state: 'approved',
+        artist_id: 'artistId',
+        consigned_partner_submission_id: consignment.id
+      )
+    end
+    let(:consignment) do
+      Fabricate(
+        :partner_submission,
+        state: 'open',
+        partner: partner,
+        sale_price_cents: 50
+      )
+    end
+    let(:offer) do
+      Fabricate(
+        :offer,
+        state: 'sent',
+        partner_submission: consignment,
+        offer_type: 'purchase'
+      )
+    end
+    
+    context 'when artwork submission exist' do
+      before do
+        submission.update!(source_artwork_id: '1')
+        consignment.update!(accepted_offer: offer)
+      end
+
+      it 'returns ok, and update consignment price' do
+        put :update, params: { id: '1' }
+        expect(response.body).to eq("{\"result\":\"ok\"}")
+        expect(submission.consigned_partner_submission.sale_price_cents).to eq 2
+      end
+    end
+
+    context 'when artwork submission do not exist' do
+      before do
+        submission.update!(source_artwork_id: nil)
+        consignment.update!(accepted_offer: offer)
+      end
+
+      it 'returns ok, and do not update consignment price' do
+        put :update, params: { id: '1' }
+        expect(response.body).to eq("{\"result\":\"ok\"}")
+        expect(consignment.sale_price_cents).to eq 50
+      end
+    end
+  end
+end
