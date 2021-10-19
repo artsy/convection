bfb85e95ee62fafc9142ffc512b788c2f5eccd59
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-10-19 15:05:15 +0300

    return old format to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_105312) do
+ActiveRecord::Schema.define(version: 2021_10_14_123304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_trgm'
   enable_extension 'plpgsql'
