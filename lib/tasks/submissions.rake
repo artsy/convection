9cb6406236a1aadaf56ffcd676f7eb76e5eb64a7
Gon4ar5 <stanislau.hanchar@artsymail.com>
2021-11-23 15:57:19 +0300

    add new specs

--- lib/tasks/submissions.rake
+++ lib/tasks/submissions.rake
@@ -5,18 +5,31 @@ task :exchange_assigned_to_real_user => :environment do
     submission.exchange_assigned_to_real_user!
   end
 end
+    
+task correction_seq_id: :environment do
+  ActiveRecord::Base.connection.tables.each do |t|
+    ActiveRecord::Base.connection.reset_pk_sequence!(t)
+  end
+end
 
 task :rebase_user_submission => :environment do
   User.all.find_each do |user|
+    # find all submissions with user_id like the user.id
     user_submissions = Submission.where(user_id: user.id)
     if user_submissions.count > 1
       # delete from result already assigned submission
-      user_submissions.delete(user.submission)
-      # dublicate user and assign submission
+      user_submissions = user_submissions.reject { |submission| submission == user.submission }
       user_submissions&.each do |user_submission|
-        dup_user = user.dup
-        dup_user.submission = user_submission
-        dup_user.save!
+        # create new user and assign submission
+        new_user = User.create!(
+          gravity_user_id: user.gravity_user_id,
+          name: user[:name],
+          email: user[:email],
+          phone: user[:phone],
+          session_id: user.session_id
+        )
+        new_user.submission = user_submission
+        new_user.save!
       end
     end
   end
--- spec/tasks/submission_spec.rb
+++ spec/tasks/submission_spec.rb
@@ -5,35 +5,34 @@ require 'rails_helper'
 Rails.application.load_tasks
 
 describe 'Submission rake tasks' do
-  describe 'Submission lapse sent rake task' do
-    subject(:invoke_task) do
-      Rake.application.invoke_task('rebase_user_submission')
-    end
+  subject(:invoke_task) do
+    Rake.application.invoke_task('correction_seq_id')
+    Rake.application.invoke_task('rebase_user_submission')
+  end
 
-    let!(:user1) { Fabricate(:user, gravity_user_id: 'userid', id: 1) }
-    let!(:user1_submission1) { Fabricate(:submission, user: user1) }
-    let!(:user1_submission2) { Fabricate(:submission, user: user1) }
-    let!(:user1_submission3) { Fabricate(:submission, user: user1) }
+  let!(:user1) { Fabricate(:user, gravity_user_id: 'userid', id: 1) }
+  let!(:user1_submission1) { Fabricate(:submission, user: user1) }
+  let!(:user1_submission2) { Fabricate(:submission, user: user1) }
+  let!(:user1_submission3) { Fabricate(:submission, user: user1) }
 
-    let!(:user2) { Fabricate(:user, gravity_user_id: 'userid2', id: 2) }
-    let!(:submission4) { Fabricate(:submission, user: user2) }
-    let!(:submission5) { Fabricate(:submission, user: user2) }
+  let!(:user2) { Fabricate(:user, gravity_user_id: 'userid2', id: 2) }
+  let!(:user2_submission1) { Fabricate(:submission, user: user2) }
+  let!(:user2_submission2) { Fabricate(:submission, user: user2) }
 
-    it "update the state if offer is set to 'sent' before a certain date" do
-      expect { invoke_task }.not_to(change { user1_submission1.reload.user.id })
-      expect { invoke_task }.to(
-        change { user1_submission2.reload.user.id }.from(1).to(3)
-      )
-      expect { invoke_task }.to(
-        change { user1_submission3.reload.user.id }.from(1).to(4)
-      )
+  let!(:user3) { Fabricate(:user, gravity_user_id: 'userid3', id: 3) }
+  let!(:user3_submission1) { Fabricate(:submission, user: user3) }
 
-      expect { invoke_task }.to(
-        change { user1_submission2.reload.user.id }.from(1).to(5)
-      )
-      expect { invoke_task }.to(
-        change { user1_submission3.reload.user.id }.from(1).to(6)
-      )
-    end
+  before { invoke_task }
+
+  it 'create a user for each non-unique user_id in the submission' do
+    expect(user1_submission1.reload.user.id).to eq(user1.id)
+    expect(user1_submission2.reload.user.id).to eq(4) # will create a new user
+    expect(user1_submission3.reload.user.id).to eq(5) # will create a new user
+
+    expect(user2_submission1.reload.user.id).to eq(user2.id)
+    expect(user2_submission2.reload.user.id).to eq(6) # will create a new user
+
+    expect(user3_submission1.reload.user.id).to eq(user3.id)
+    expect(Submission.count).to eq(User.count)
   end
 end
