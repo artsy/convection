d2809848ec5afc1ab9ab30774dceaf6f0a98e414
Alican Akyuz <alicakyz@gmail.com>
2021-11-24 10:44:03 +0100

    user fabricator

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -5,5 +5,4 @@ Fabricator(:user) do
     Fabricate.sequence(:gravity_user_id) { |i| "user-id-#{i}" }
   end
   email { Fabricate.sequence(:email) { |i| "jon-jonson#{i}@test.com" } }
-  session_id { Fabricate.sequence(:session_id) { |i| "session_id#{i}" } }
 end
