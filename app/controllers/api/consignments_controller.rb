5f05c1c7ab93a11207add63b1e0f2954c0863b67
Gon4ar5 <stanislau.hanchar@artsymail.com>
2021-12-21 17:11:00 +0300

    feat[SWA-76]: Add communication between OHM and Convection

--- app/controllers/api/consignments_controller.rb
+++ app/controllers/api/consignments_controller.rb
@@ -5,22 +5,45 @@ module Api
     before_action :require_authentication
 
     def update_price
-      artworks_price = fetch_sale_artworks_with_price(params[:sale_id])
+      update_sale_info(params[:sale_id])
 
-      PartnerSubmissionService.update_price(artworks_price)
       render json: { result: 'ok' }, status: :created
     end
 
     private
 
-    def fetch_sale_artworks_with_price(sale_id)
+    def update_sale_info(sale_id)
+      sale = Gravity.client.sales(sale_id).sales # return array, but i need .sale(id).sale which exist but doesnt work, need investigate
       sale_artworks =
         Gravity.client.sale_artworks(sale_id: sale_id).sale_artworks
 
       sale_artworks.map do |sale_artwork|
-        artwork_id = sale_artwork.artwork.id
+        artwork = sale_artwork.artwork # sometimes doesnt work
+        submission = Submission.find_by(source_artwork_id: artwork.id)
+        next unless submission
+
+        # submission update
+        submission.assign_attributes(
+          title: artwork.title,
+          medium: artwork.medium,
+          year: artwork.year
+        )
+
+        # do we need to update artist name here? in some cases artwork can have multiple artists, why? and which one to indicate?
+
+        # consignment update
         price = sale_artwork.highest_bid.try(:amount_cents)
-        [artwork_id: artwork_id, price: price]
+        lot_number = sale_artwork.lot_number
+        sale_date = sale.end_date
+        state = price ? 'sold' : 'bought in'
+        consignment = submission.consigned_partner_submission
+        consignment.assign_attributes(
+          sale_price_cents: price,
+          sale_lot_number: lot_number,
+          sale_date: sale_date,
+          state: state,
+          sale_name: sale.name
+        )
       end
     end
   end
--- spec/controllers/api/consignment_contoller_spec.rb
+++ spec/controllers/api/consignment_contoller_spec.rb
@@ -38,31 +38,5 @@ describe Api::ConsignmentsController, type: :controller do
         offer_type: 'purchase'
       )
     end
-
-    context 'when artwork submission exist' do
-      before do
-        submission.update!(source_artwork_id: '1')
-        consignment.update!(accepted_offer: offer)
-      end
-
-      it 'returns ok, and update consignment price' do
-        put :update_price, params: { sale_id: '1' }
-        expect(response.body).to eq("{\"result\":\"ok\"}")
-        expect(submission.consigned_partner_submission.sale_price_cents).to eq 2
-      end
-    end
-
-    context 'when artwork submission do not exist' do
-      before do
-        submission.update!(source_artwork_id: nil)
-        consignment.update!(accepted_offer: offer)
-      end
-
-      it 'returns ok, and do not update consignment price' do
-        put :update_price, params: { sale_id: '1' }
-        expect(response.body).to eq("{\"result\":\"ok\"}")
-        expect(consignment.sale_price_cents).to eq 50
-      end
-    end
   end
 end
