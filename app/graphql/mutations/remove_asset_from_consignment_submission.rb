291458198c1a18aaf58c256f5ea95647f60710ff
Alican Akyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2021-12-23 15:34:55 +0100

    [feat]: Create RemoveAssetFromConsignmentSubmission mutation (#1199)
    * feat: create RemoveAssetFromConsignmentSubmission mutation
    
    * feat: add destroy method to api assets controller
    
    * arrange args needed for removal
    
    * arrange typing
    
    * test
    
    * finalise resolver
    
    * dumb schema
    
    * test remove asset
    
    * remove some graphql mutation attr
    
    Co-authored-by: Gon4ar5 <stanislau.hanchar@artsymail.com>

---
+++ app/graphql/mutations/remove_asset_from_consignment_submission.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Mutations
+  class RemoveAssetFromConsignmentSubmission < Mutations::BaseMutation
+    argument :sessionID, String, required: false
+    argument :assetID, String, required: false
+
+    field :asset, Types::AssetType, null: true
+
+    def resolve(arguments)
+      resolve_options = {
+        arguments: arguments,
+        context: context,
+        object: object
+      }
+      resolver = RemoveAssetFromSubmissionResolver.new(resolve_options)
+
+      resolver.run
+    end
+  end
+end
