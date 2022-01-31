3133959b8a60b21b5556db4a7516233ca445e1e5
Kirill Zuborev <kiry.zubarau@artsymail.com>
2022-01-31 17:36:01 +0300

    Update models relationships

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class AdminUser < ApplicationRecord
+  has_many :submissions,
+           dependent: :nullify,
+           inverse_of: :created_by,
+           foreign_key: 'created_by_id'
+
   validates :gravity_user_id, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
 
