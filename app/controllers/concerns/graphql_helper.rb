626b1296b9684d8d3ca2ff28cf3adf1fe23deb2e
Gon4ar5 <stanislau.hanchar@artsymail.com>
2022-02-18 15:44:46 +0300

    Fix[SWA-150]: Naming mistake

--- app/controllers/concerns/graphql_helper.rb
+++ app/controllers/concerns/graphql_helper.rb
@@ -53,7 +53,7 @@ module GraphqlHelper
     submission.update(
       my_collection_artwork_id:
         response.dig(
-          :date,
+          :data,
           :myCollectionCreateArtwork,
           :artworkOrError,
           :artworkEdge,
