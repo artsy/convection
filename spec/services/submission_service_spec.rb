5b78badc01852799ee18be763aa29819cf40171e
Kizito Egeonu <kizito.egeonu@gmail.com>
2022-11-28 15:06:22 +0100

    update copy of rejection email

--- spec/services/submission_service_spec.rb
+++ spec/services/submission_service_spec.rb
@@ -481,7 +481,7 @@ describe SubmissionService do
       expect(emails.first.to).to eq(%w[michael@bluth.com])
       expect(emails.first.from).to eq(%w[consign@artsy.net])
       expect(emails.first.html_part.body).to include(
-        'we unfortunately don’t have demand for this artwork on Artsy at the moment.'
+        "Unfortunately, we don’t have a selling opportunity for this work right now."
       )
       expect(submission.state).to eq 'rejected'
       expect(submission.rejection_reason).to eq 'NSV'
