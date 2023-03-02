ee05eedc46772a0eac6250256ee1889dfec9b4c1
Kizito Egeonu <kizito.egeonu@gmail.com>
2023-03-02 16:50:52 +0100

    Feat(CX-3410): Add Recipient Email to Consignments Co-authored-by: Daria Koslova <daria.Kozlova@artsymail.com>

--- app/models/consignment_inquiry.rb
+++ app/models/consignment_inquiry.rb
@@ -1,5 +1,6 @@
 class ConsignmentInquiry < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :recipient_email, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_nil: true
   validates :name, presence: { message: "is required" }
   validates :message, presence: { message: "is required" }
 end
