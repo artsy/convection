c265a968f5f2e5d5aff081fbcb200dcf853372a6
Ole <ole.richter91@gmail.com>
2023-07-06 14:39:05 +0200

    add partner test

--- spec/requests/api/graphql/queries/submissions_spec.rb
+++ spec/requests/api/graphql/queries/submissions_spec.rb
@@ -87,6 +87,23 @@ describe 'submissions query' do
         expect(error_message).to eq "Can't load other people's submissions."
       end
     end
+
+    context 'with a request from a partner user and wrong submission id' do
+      let(:token) { partner_token }
+
+      it 'returns an error for that request' do
+        post '/api/graphql', params: { query: query }, headers: headers
+
+        expect(response.status).to eq 200
+        body = JSON.parse(response.body)
+
+        submissions_response = body['data']['submissions']
+        expect(submissions_response).to eq nil
+
+        error_message = body['errors'][0]['message']
+        expect(error_message).to eq "Can't load other people's submissions."
+      end
+    end
   end
 
   describe 'valid requests' do
