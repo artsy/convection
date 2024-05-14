5498b52a856d3e430cc2c5a4203604b3621716a3
Nikita Skalkin <nikskalkin@gmail.com>
2024-05-14 15:41:32 +0200

    spike(onyx-910): submission page renders (partially) artwork details backed by Metaphysics

--- app/controllers/admin/submissions_controller.rb
+++ app/controllers/admin/submissions_controller.rb
@@ -79,6 +79,8 @@ module Admin
     end
 
     def show
+      set_artwork_details!
+
       notified_partner_submissions =
         @submission.partner_submissions.where.not(notified_at: nil)
       @partner_submissions_count =
@@ -198,6 +200,38 @@ module Admin
 
     def set_submission_artist
       @artist = artists_details_query([@submission.artist_id]).first
+    end
+
+    def set_artwork_details!
+      return unless @submission.my_collection_artwork_id
+
+      response = artwork_details_query(@submission.my_collection_artwork_id, session[:access_token])
+      return unless response
+
+      @artwork_details = OpenStruct.new(
+        artist_name: response.try(:[], :artist).try(:[], :name),
+        title: response.try(:[], :title),
+        signature: response.try(:[], :signature),
+        category: response.try(:[], :mediumType).try(:[], :name),
+        medium: response.try(:[], :medium),
+        edition_size: response.try(:[], :editionSize),
+        edition_number: response.try(:[], :editionNumber),
+        dimensions_in: response.try(:[], :dimensions).try(:[], :in),
+        dimensions_cm: response.try(:[], :dimensions).try(:[], :cm),
+        year: response.try(:[], :date),
+        provenance: response.try(:[], :provenance),
+        has_certificate_of_authenticity: response.try(:[], :hasCertificateOfAuthenticity) || "No",
+        certificate_of_authenticity_details: response.try(:[], :certificateOfAuthenticity).try(:[], :details),
+        coa_by_authenticating_body: false, # TODO: Not yet supported
+        coa_by_gallery: false, # TODO: Not yet supported
+        location: "-", # Not yet supported
+        price_in_mind: response.try(:[], :pricePaid).try(:[], :display),
+        is_p1: response.try(:[], :artist).try(:[], :targetSupply).try(:[], :isP1),
+        target_supply: response.try(:[], :artist).try(:[], :targetSupply).try(:[], :isTargetSupply),
+        images: response.try(:[], :images)&.map do |image|
+          OpenStruct.new( url: image.try(:[], :resized).try(:[], :url), default: image.try(:[], :isDefault) )
+        end
+      )
     end
 
     def submission_params
