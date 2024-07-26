ff57274060ad9db4ddbcf0b2f6d0e272b534bfc1
Nikita Skalkin <nikskalkin@gmail.com>
2024-07-26 13:40:58 +0200

    code review fix

--- app/models/submission_state_actions.rb
+++ app/models/submission_state_actions.rb
@@ -16,7 +16,7 @@ class SubmissionStateActions
     when Submission::DRAFT, Submission::SUBMITTED
       [approve_action, publish_action, hold_action, reject_action, close_action]
     when Submission::RESUBMITTED
-      [publish_action, hold_action, reject_action, close_action]
+      [hold_action, reject_action, close_action]
     when Submission::APPROVED
       [publish_action, hold_action, close_action]
     when Submission::PUBLISHED
